85ed66287b57db4ab8b797be1e1bac40ee625f6b
jacky <jackylvm@foxmail.com>
2018-12-21 11:22:51 +0800

    去除物理引擎设置,暂时用不到物理引擎

--- assets/script/physics/PhysicsSetting.js
+++ assets/script/physics/PhysicsSetting.js
@@ -3,7 +3,7 @@
  * File Created: 2018-12-08 18:18:29
  * Author: Jacky (jackylvm@foxmail.com>)
  * -----
- * Last Modified: 2018-12-19 16:51:27
+ * Last Modified: 2018-12-21 11:22:08
  * Modified By: Jacky (jackylvm@foxmail.com>)
  * -----
  * Copyright 2018 上海火刀石网络科技有限公司
@@ -14,15 +14,15 @@
  */
 // 该文件不需要绑定在什么地方,creator启动后会自动执行
 cc.game.on(cc.game.EVENT_ENGINE_INITED, function (evt) {
-    let physicsMgr = cc.director.getPhysicsManager();
+    // let physicsMgr = cc.director.getPhysicsManager();
 
-    physicsMgr.enabled = true;
-    physicsMgr.gravity = cc.v2(0, 0);
+    // physicsMgr.enabled = true;
+    // physicsMgr.gravity = cc.v2(0, 0);
 
-    physicsMgr.debugDrawFlags =
-        cc.PhysicsManager.DrawBits.e_jointBit |
-        cc.PhysicsManager.DrawBits.e_shapeBit |
-        cc.PhysicsManager.DrawBits.e_aabbBit |
-        cc.PhysicsManager.DrawBits.e_pairBit |
-        cc.PhysicsManager.DrawBits.e_centerOfMassBit;
+    // physicsMgr.debugDrawFlags =
+    //     cc.PhysicsManager.DrawBits.e_jointBit |
+    //     cc.PhysicsManager.DrawBits.e_shapeBit |
+    //     cc.PhysicsManager.DrawBits.e_aabbBit |
+    //     cc.PhysicsManager.DrawBits.e_pairBit |
+    //     cc.PhysicsManager.DrawBits.e_centerOfMassBit;
 });
