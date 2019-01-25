c773783412cafbf4f3c852e311c4f27bd1ad9543
jacky <jackylvm@foxmail.com>
2019-01-25 12:19:12 +0800

    重新调整示例结构

---
+++ assets/script/demo/CDemoSeek.js
@@ -0,0 +1,51 @@
+/*
+ * File: CDemo01.js
+ * File Created: 2018-12-18 20:07:08
+ * Author: Jacky (jackylvm@foxmail.com>)
+ * -----
+ * Last Modified: 2019-01-25 12:13:11
+ * Modified By: Jacky (jackylvm@foxmail.com>)
+ * -----
+ * Copyright 2018 上海火刀石网络科技有限公司
+ * -----
+ * HISTORY:
+ * Date      			By			Comments
+ * --------------------	---------	----------------------------------
+ */
+import CMousePoint from "CMousePoint";
+import CWorld from "CWorld";
+import {
+    EnumBehaviorType
+} from "CEnum";
+
+cc.Class({
+    extends: CWorld,
+
+    properties: {
+        MousePointInst: {
+            default: null,
+            type: CMousePoint
+        }
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+    // onLoad () {},
+    start() {
+        var self = this;
+
+        self._super();
+
+        self.MousePointInst.init(self);
+
+        var _tmp = self.MousePointInst.node.getPosition();
+        _tmp = self.MousePointInst.node.parent.convertToWorldSpaceAR(_tmp);
+        self._crosshair = self.vehicle.parent.convertToNodeSpaceAR(_tmp);
+
+        self.cVehicle.changeBehavior(EnumBehaviorType.SEEK);
+    },
+    // update (dt) {},
+    onMousePointMove(worldPt) {
+        var self = this;
+        self._crosshair = self.vehicle.parent.convertToNodeSpaceAR(worldPt);
+    },
+});
---
+++ assets/script/world/CWorld.js
@@ -0,0 +1,74 @@
+/*
+ * File: CWorld.js
+ * File Created: 2019-01-25 10:47:54
+ * Author: Jacky (jackylvm@foxmail.com>)
+ * -----
+ * Last Modified: 2019-01-25 12:12:54
+ * Modified By: Jacky (jackylvm@foxmail.com>)
+ * -----
+ * Copyright 2019 上海火刀石网络科技有限公司
+ * -----
+ * HISTORY:
+ * Date      			By			Comments
+ * --------------------	---------	----------------------------------
+ */
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        VehiclePrefab: {
+            default: null,
+            type: cc.Prefab
+        },
+        VehicleNode: {
+            default: null,
+            type: cc.Node
+        }
+    },
+
+    // onLoad () {},
+    start() {
+        var self = this;
+
+        self.vehicle = cc.instantiate(self.VehiclePrefab);
+        self.cVehicle = self.vehicle.getComponent("CVehicle");
+        self.cVehicle.initVehicle(self);
+        self.VehicleNode.addChild(self.vehicle);
+    },
+    // update (dt) {},
+    walls() {
+        return [];
+    },
+    obstacles() {
+        return [];
+    },
+    crosshair() {
+        var self = this;
+        return self._crosshair;
+    },
+    tagObstacleWithinViewRange(vehicle, range) {
+        var self = this;
+
+        self.tagNeighbors(vehicle, self._vehicles, range);
+    },
+    tagNeighbors(vehicle, neighbors, radius) {
+        var self = this;
+
+        for (let i = 0; i < neighbors.length; i++) {
+            const _cVehicle = neighbors[i];
+            _cVehicle.untag();
+
+            if (vehicle != _cVehicle) {
+                var _to = _cVehicle.pos().sub(_cVehicle.pos());
+                var _range = radius + _cVehicle.bRadius();
+                if (_to.magSqr() < (_range * _range)) {
+                    _cVehicle.tag();
+                }
+            }
+        }
+    },
+    agents() {
+        var self = this;
+        return self._vehicles;
+    },
+});
