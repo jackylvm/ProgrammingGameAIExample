f0dc2fa94b4e020648a41b6c720e215d0f336bc9
jacky <jackylvm@foxmail.com>
2019-01-25 14:34:30 +0800

    把绘图分开,因为除了实体本身,还有其他东西需要绘制

--- assets/script/prefab/CVehicle.js
+++ assets/script/prefab/CVehicle.js
@@ -3,7 +3,7 @@
  * File Created: 2018-12-18 19:52:15
  * Author: Jacky (jackylvm@foxmail.com>)
  * -----
- * Last Modified: 2018-12-21 11:00:20
+ * Last Modified: 2019-01-25 14:29:25
  * Modified By: Jacky (jackylvm@foxmail.com>)
  * -----
  * Copyright 2018 上海火刀石网络科技有限公司
@@ -20,10 +20,20 @@ import {
 cc.Class({
     extends: CVehicleEntity,
 
-    properties: {},
+    properties: {
+        body: {
+            default: null,
+            type: cc.Graphics
+        },
+        wander: {
+            default: null,
+            type: cc.Graphics
+        }
+    },
 
     initVehicle(world) {
         var self = this;
+
         self.initVehicleEntity(world, new cc.v2(0, 0), EnumConst.HALF_PI, new cc.v2(0, 0), 1, 100, 120, 60, 1);
     },
     // LIFE-CYCLE CALLBACKS:
@@ -31,16 +41,14 @@ cc.Class({
     start() {
         var self = this;
 
-        var _graphics = self.getComponent(cc.Graphics);
+        self.body.clear();
 
-        _graphics.clear();
-
-        _graphics.moveTo(-6, 4);
-        _graphics.lineTo(6, 0);
-        _graphics.lineTo(-6, -4);
-        _graphics.lineTo(-6, 4);
-        _graphics.circle(0, 0, 1);
-        _graphics.stroke();
+        self.body.moveTo(-6, 4);
+        self.body.lineTo(6, 0);
+        self.body.lineTo(-6, -4);
+        self.body.lineTo(-6, 4);
+        self.body.circle(0, 0, 1);
+        self.body.stroke();
     },
-    // update (dt) {},
+    // update(dt) {},
 });
